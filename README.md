7b50ec56470cd26f740aa5683298a1dbb22bf609
Andy Mehalick <andy.mehalick@orangejetpack.com>
2018-09-24 09:59:53 +0300

    add docs project

--- README.md
+++ README.md
@@ -52,21 +52,19 @@ public async Task<IActionResult> Index()
 
 For ASP.NET Core projects you'll add the **Xaki** and **Xaki.AspNetCore** NuGet packages to your project. While these packages are beta you'll install from MyGet:
 
+##### Package Manager
+
 ```powershell
-dotnet add package Xaki.AspNetCore --version 0.0.1-* --source https://www.myget.org/F/xaki/api/v3/index.json
+Install-Package Xaki
+```
+
+##### .NET CLI
+
+```powershell
+dotnet add package Xaki
 ```
 
 You may also want to add the NuGet feed above to your nuget.config file at the root of your solution:
-
-```xml
-<?xml version="1.0" encoding="utf-8"?>
-<configuration>
-  <packageSources>
-    <add key="MyGet" value="https://www.myget.org/F/xaki/api/v3/index.json" />
-    <add key="NuGet" value="https://api.nuget.org/v3/index.json" />
-  </packageSources>
-</configuration>
-```
 
 #### 2. Add Xaki to Startup
 
@@ -75,23 +73,21 @@ Xaki follows the usual pattern to add and configure services in an ASP.NET Core 
 ```csharp
 public void ConfigureServices(IServiceCollection services)
 {
-    services.AddXaki(new XakiOptions
+    // ...
+
+    services.AddMvc().AddXaki(new XakiOptions
     {
         RequiredLanguages = new List<string> { "en", "zh", "ar", "es", "hi" },
         OptionalLanguages = new List<string> { "pt", "ru", "ja", "de", "el" }
     });
-
-    services.AddMvc().AddXakiMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-    // ...
 }
 
 public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 {
-    var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
-    app.UseRequestLocalization(options.Value);
-
     // ...
+
+    app.UseXaki(); // must precede UseMvc()
+    app.UseMvc();
 }
 ```
 
---
+++ docs/Xaki.Docs/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Xaki.Docs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCaching();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts(options => options.MaxAge(365).IncludeSubdomains());
+            }
+
+            app.UseHttpsRedirection();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXContentTypeOptions();
+            app.UseXfo(options => options.Deny());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseCsp(options => options
+                .BlockAllMixedContent()
+                .DefaultSources(c => c.Self())
+                .FontSources(s => s.CustomSources("data:", "https://cdnjs.cloudflare.com", "https://fonts.gstatic.com"))
+                .ImageSources(s => s.Self().CustomSources("data:", "https://xaki.azureedge.net"))
+                .ScriptSources(s => s.Self().UnsafeInline().CustomSources("https://cdnjs.cloudflare.com"))
+                .StyleSources(s => s.Self().UnsafeInline().CustomSources("https://cdnjs.cloudflare.com")));
+
+            app.UseResponseCaching();
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.GetTypedHeaders().CacheControl =
+                    new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromSeconds(10)
+                    };
+                context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+                    new[] { "Accept-Encoding" };
+
+                await next();
+            });
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
