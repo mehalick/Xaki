7f6c6dac08c701a9398097a2f5e4f0ce4aec5726
Andy Mehalick <mehalick@gmail.com>
2018-10-05 15:40:30 +0300

    add benchmarks project

---
+++ benchmarks/Xaki.Benchmarks/Models/Moon.cs
@@ -0,0 +1,14 @@
+﻿namespace Xaki.Benchmarks.Models
+{
+    public class Moon : ILocalizable
+    {
+        public int MoonId { get; set; }
+
+        public int PlanetId { get; set; }
+
+        [Localized]
+        public string Name { get; set; }
+
+        public virtual Planet Planet { get; set; }
+    }
+}
