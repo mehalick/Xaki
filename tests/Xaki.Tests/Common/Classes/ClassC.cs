42131ddb96a357b9e32a9610a4ec725588a8e439
Tarek <6104018+Tarek-Samy@users.noreply.github.com>
2018-10-12 06:02:29 +0200

    Deep localization with tests (#15)
    * Deep localization with item checking
    
    * Add coment
    
    * ..
    
    * parallel projection
    
    * unit test wip
    
    * Better reflection usage
    
    * Add Shallow tests; Add OneLevel tests;
    
    * Deep localization tetst; comments;

---
+++ tests/Xaki.Tests/Common/Classes/ClassC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xaki.Tests.Common.Classes.Abstractions;
+
+namespace Xaki.Tests.Common.Classes
+{
+    public class ClassC : ILocalizable, ITestClass
+    {
+        [Localized]
+        public string Name { get; set; }
+
+        public ClassA A { get; set; }
+
+        public List<ClassA> ListOfA { get; set; } = new List<ClassA>(0);
+
+        public ClassB B { get; set; }
+
+        public List<ClassB> ListOfB { get; set; } = new List<ClassB>(0);
+
+        public ClassC(string name)
+        {
+            Name = name;
+        }
+    }
+}
